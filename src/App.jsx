8d8db33397892fb15f229a59401deedaeea236f4
chiench <quangminhb53@gmail.com>
2023-10-09 11:17:28 +0700

    feat: create route and views

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-     
+     App
     </>
   )
 }
---
+++ src/router.jsx
@@ -0,0 +1,52 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "./views/Auth/login";
+import Register from "./views/Auth/register";
+import User from "./views/User/user";
+import Dashboard from "./views/dashboard";
+import Defaultlayout from "./components/defaultlayout";
+import Guestlayout from "./components/guestlayout";
+import Notfound from "./views/notfound";
+
+const router = createBrowserRouter([
+    { 
+        path: "/",
+        element: <Guestlayout />,
+        children: [
+            { 
+                path: "/login",
+                element: <Login />
+            },
+            { 
+                path: "/register",
+                element: <Register />
+            },
+        ]
+    },
+    { 
+        path: "/",
+        element: <Defaultlayout />,
+
+        children: [
+            { 
+                
+                path: "/",
+                element: <Navigate to="users" />
+            },
+            { 
+
+                path: "/users",
+                element: <User />
+            },
+            { 
+                path: "/dashboard",
+                element: <Dashboard />
+            },
+        ]
+    },
+    { 
+        path: "*",
+        element: <Notfound />
+    },
+])
+
+export default router;
